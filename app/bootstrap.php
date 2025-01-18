121e8a9f2fcfffeb90262a0b06830289695945ea
sanjomaru <sanjomaruwork13@gmail.com>
2025-01-18 23:01:07 +0800

    - Modify Core.php that gets the url value and - accesing the contoller dynamically through - urls.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once('libraries/core.php');
+
+new Core;
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -6,21 +6,34 @@
 */
 
 class Core{
-    protected $currentController = 'pages';
+    protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
 
     public function __construct(){
-       
-        return $this->getUrl();
+       $url = $this->getUrl();
+        
+       if(file_exists('../app/controller/'.ucwords($url[0]).'.php')){
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+       }
+       require_once('../app/controller/'.$this->currentController.'.php');
+       $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        } else {
+            return $url = $this->currentController;
+        }
     }
 
 }
 
-new Core;
+
 
 ?>
