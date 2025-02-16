73bcbfd5e00c1b4d7086bf10df135c39b45fcf86
sanjomaru <sanjomaruwork13@gmail.com>
2025-02-16 23:54:17 +0800

    - Continue working on the register form

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -17,8 +17,33 @@ $description = $data['description'];
     <div class="row">
         <form action="<?php echo URLROOT; ?>/users/register" method="post">
             <div class="form-group">
-                <label for="name">Name</label>
+                <label for="name">Name: </label>
                 <input type="text" name="name" id="" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
+                <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="text" name="email" id="" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+                <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="password">Password: </label>
+                <input type="text" name="password" id="" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+                <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">Confirm Password: </label>
+                <input type="text" name="confirm_password" id="" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
+                <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <input type="submit" value="Register" class='btn btn-success btn-block'>
+                </div>
+                <div class="col">
+                    <a href="<?php echo URLROOT.'/users/login'; ?>" class='btn btn-light btn-block'>Have an account? Login</a>
+                </div>
             </div>
         </form>
     </div>
